d41f977ff2a7a93900525d803df4dd908bec1f8d
math231k <54638811+math231k@users.noreply.github.com>
2021-05-03 08:54:35 +0200

    cors policy problems

--- src/app/forums/forums.component.ts
+++ src/app/forums/forums.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ForumService} from './shared/forum.service';
+import {debounceTime, takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
+import {Category} from '../shared/models/category';
 
 @Component({
   selector: 'app-forums',
   templateUrl: './forums.component.html',
   styleUrls: ['./forums.component.scss']
 })
-export class ForumsComponent implements OnInit {
+export class ForumsComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  categories: Category[] = [];
+  unsubscriber$ = new Subject();
+  constructor(private service: ForumService) { }
 
   ngOnInit(): void {
+    this.service.getCategores().
+    pipe(
+      takeUntil(this.unsubscriber$)
+    )
+      .subscribe((categories) => {
+        this.categories = categories;
+      });
 
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber$.next();
+    this.unsubscriber$.complete();
   }
 
 }
